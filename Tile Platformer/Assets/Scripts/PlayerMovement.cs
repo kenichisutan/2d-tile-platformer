8b882432a680b4ddf47633b9a9ba7f95abbccaad
Juan Kenichi Sutan <kenichi.jks@gmail.com>
2022-08-16 16:53:46 +0900

    Added to animations
    Sprites now flip depending on direction of velocity.

--- Tile Platformer/Assets/Scripts/PlayerMovement.cs
+++ Tile Platformer/Assets/Scripts/PlayerMovement.cs
@@ -10,11 +10,13 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
+    Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,6 +26,7 @@ public class PlayerMovement : MonoBehaviour
         // float jumpAmount = Input.GetAxis("Jump") * jumpSpeed * Time.deltaTime;
         // transform.Translate(moveAmount, jumpAmount, 0);
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
@@ -43,7 +46,27 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
+        // if(Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon)
+        // {
+        //     myAnimator.SetBool("isRunning", true);
+        // }
+        // else
+        // {
+        //     myAnimator.SetBool("isRunning", false);
+        // }
+
+        bool hasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", hasHorizontalSpeed);
     }
 
+    void FlipSprite()
+    {
+        bool hasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if(hasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
 
 }
